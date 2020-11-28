6d7e5fad37bab01ca2c306fa7cb9e83eba5ed743
Mateus da Rosa Zanatta <mateus_zanatta@hotmail.com>
2020-11-28 14:39:32 -0300

    Began working on the Dashboard page

--- web/src/components/Sidebar.tsx
+++ web/src/components/Sidebar.tsx
@@ -3,6 +3,8 @@ import {FiArrowLeft} from 'react-icons/fi';
 import { useHistory } from 'react-router-dom';
 
 import mapMarkerImg from '../images/map-marker.svg';
+import ApprovedImg from '../images/approved.svg';
+import waitingImg from '../images/waiting.svg';
 
 import '../styles/components/Sidebar.css';
 
@@ -11,7 +13,14 @@ export default function Sidebar(){
     return(
         <aside className='app-sidebar'>
             <img src={mapMarkerImg} alt="Happy" />
-
+            <div>
+                <button type="button" id='upperbutton'>
+                    <img src={ApprovedImg} alt="Happy" />
+                </button>
+                <button type="button">
+                    <img src={waitingImg} alt="Happy" />
+                </button>
+            </div>
             <footer>
             <button type="button" onClick={goBack}>
                 <FiArrowLeft size={24} color="#FFF" />
--- web/src/pages/OrphanageDashboard.tsx
+++ web/src/pages/OrphanageDashboard.tsx
@@ -1,8 +1,23 @@
 import React, { ChangeEvent, FormEvent, useState } from "react";
 import { useHistory } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
+
+import '../styles/pages/orphanage-dashboard.css'
 
 export default function OrphanageDashboard() {
     const history = useHistory();
 
-    return (<div></div>);
+    return (
+        <div id="page-orphanage-dashboard">
+            <Sidebar />
+            <div id="orphanage-title"> 
+                <h2>Orfanatos Cadastrados</h2>
+                <p>2 Orfanatos</p>
+            </div>
+            
+            <main>
+                
+            </main>
+        </div>
+    );
 }
